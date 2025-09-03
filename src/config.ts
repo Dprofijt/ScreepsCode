2109d845ec3a170045140823e7ad0e9d7a938fd9
Dprofijt <44891295+Dprofijt@users.noreply.github.com>
2025-09-03 23:50:08 +0200

    upgraders changed to 1

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const MAXHARVESTERS = 2;
 export const MAXBUILDERS = 1;
 export const MAXMOVERS = 1;
-export const MAXUPGRADERS = 2;
+export const MAXUPGRADERS = 1;
 
 export const MAXCLOSECOMBATS = 2;
 export const MAXRANGEDCOMBATS = 1;
